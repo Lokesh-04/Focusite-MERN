167f2097a9a0b12324cee0e4383892c50f979839
lokesh-04 <kancharapulokeshkumar@gmail.com>
2022-11-28 18:56:32 +0530

    complete login page

---
+++ script.js
@@ -0,0 +1,9 @@
+// play button change to pause
+function play(){
+
+
+
+
+    document.getElementById("pa").setAttribute("class","fa-solid fa-circle-pause")
+    document.getElementById("pa").innerHTML="pause"
+}
