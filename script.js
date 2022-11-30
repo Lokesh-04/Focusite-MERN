ce7e1831be484395ffcfc1ac915da68a7f3336e5
lokesh-04 <kancharapulokeshkumar@gmail.com>
2022-11-30 09:17:36 +0530

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 // play button change to pause
-function play(){
+// function play(){
 
 
 
 
-    document.getElementById("pa").setAttribute("class","fa-solid fa-circle-pause")
-    document.getElementById("pa").innerHTML="pause"
+//     document.getElementById("pa").setAttribute("class","fa-solid fa-circle-pause")
+//     document.getElementById("pa").innerHTML="pause"
+// }
+
+// signup and login pages pop-ups
+
+function signuptoggle() {
+    let blur=document.getElementById('blur');
+    blur.classList.toggle('active');
+    let popup = document.getElementById('signup-box');
+    popup.classList.toggle('active');
+}
+function logintoggle() {
+    let blur=document.getElementById('blur');
+    blur.classList.add('active');
+    let popup = document.getElementById('login-box');
+    popup.classList.add('active');
+}
+
+// x-mark code to close login or signup tabs
+
+function hide(){
+    let x=document.getElementById('x-mark');
+    x.classList.remove('active');
 }
