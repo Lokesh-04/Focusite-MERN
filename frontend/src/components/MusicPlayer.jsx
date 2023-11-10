a2df94c1f6238ddb608a453c98a54b806c306520
Lokesh-04 <98595361+Lokesh-04@users.noreply.github.com>
2023-11-10 17:05:39 +0530

    added music player and timer components

---
+++ frontend/src/components/MusicPlayer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MusicPlayer = () => {
+  return (
+    <div>
+      <h1>Music Player</h1>
+    </div>
+  )
+}
+
+export default MusicPlayer
---
+++ frontend/src/components/Timer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Timer = () => {
+  return (
+    <div>
+      <h1>Timer</h1>
+    </div>
+  )
+}
+
+export default Timer
