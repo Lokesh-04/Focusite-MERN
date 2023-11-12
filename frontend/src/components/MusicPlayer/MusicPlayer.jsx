098b6b4225a2bb9583ee37b0d9d1e14a6afb20aa
Lokesh-04 <98595361+Lokesh-04@users.noreply.github.com>
2023-11-12 14:38:27 +0530

    starting timer

--- frontend/src/components/MusicPlayer/MusicPlayer.jsx
+++ frontend/src/components/MusicPlayer/MusicPlayer.jsx
@@ -5,7 +5,6 @@ import audioFile from '../../assets/audio/audio1.mp3';
 const MusicPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const audioPlayer = useRef(null);
-  audioPlayer.volume = 0.5;
 
   const handleTogglePlay = () => {
     setIsPlaying(!isPlaying);
