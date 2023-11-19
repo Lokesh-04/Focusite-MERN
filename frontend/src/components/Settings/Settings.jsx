68f23213e5898a238ab7bb95e0c17688e51a5d6e
Lokesh-04 <98595361+Lokesh-04@users.noreply.github.com>
2023-11-19 18:50:08 +0530

    added settings pop-up

--- frontend/src/components/Settings/Settings.jsx
+++ frontend/src/components/Settings/Settings.jsx
@@ -1,26 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Timer, FadersHorizontal } from "@phosphor-icons/react";
 import "./settings.css"
 import TimerSettings from './TimerSettings/TimerSettings'
 import MusicSettings from './MusicSettings/MusicSettings'
 
 const Settings = () => {
+
+  const [isTimerSettings, setIsTimerSettings] = useState(false);
+  const [isMusicSettings, setIsMusicSettings] = useState(false);
+
+
+  const showTimerSettings = () =>{
+    if(isMusicSettings){setIsMusicSettings(!isMusicSettings);}
+    setIsTimerSettings(!isTimerSettings);
+  }
+
+  const showMusicSettings = () =>{
+    if(isTimerSettings){setIsTimerSettings(!isTimerSettings);}
+    setIsMusicSettings(!isMusicSettings);
+  }
+
+
+
   return (
     <div>
 
     <div id="all-settings">
         <div id="music-settings" className='settings'>
-        <FadersHorizontal size={32} color='white' weight="thin" />
+        <FadersHorizontal size={32} color='white' weight="thin" onClick={showMusicSettings}/>
         </div>
         <div id="timer-settings" className='settings'>
-        <Timer size={32} color='white' weight="thin" />
+        <Timer size={32} color='white' weight="thin" onClick={showTimerSettings}/>
         </div>
     </div>    
     {/* Music Settings */}
-    <div id="music-settings-content"><MusicSettings /></div>    
+    <div id="music-settings-content" style={{display: isMusicSettings? 'block': 'none'}}><MusicSettings /></div>    
 
     {/* Timer Settings */}
-    <div id="timer-settings-content"><TimerSettings /></div>
+    <div id="timer-settings-content" style={{display: isTimerSettings? 'block': 'none'}}><TimerSettings /></div>
 
     </div>
   )
--- frontend/src/components/Settings/TimerSettings/TimerSettings.jsx
+++ frontend/src/components/Settings/TimerSettings/TimerSettings.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import {React, useState} from "react";
+import axios from "axios";
+
+const backendUrl = "/api/timersettings";
 
 const TimerSettings = () => {
   return (
     <div>
-        <p>Timer settings</p>
-    </div>
-  )
-}
+      <h1>Timer Settings</h1>
+      <p>
+        Recommended work: 25min, break: 5min, longbreak: 15min</p>
+      {/* <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          name="work"
+          onChange={handleInput}
+          value={formData.work}
+          id="work"
+        />
 
-export default TimerSettings
+        <br />
+
+        <input
+          type="number"
+          name="break"
+          onChange={handleInput}
+          value={formData.break}
+          id="break"
+        />
+
+        <br />
+
+        <input
+          type="number"
+          name="longbreak"
+          onChange={handleInput}
+          value={formData.longbreak}
+          id="longbreak"
+        />
+
+        <br />
+
+        <button type="submit">Save</button>
+      </form> */}
+
+
+  {/* <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="data"
+            onChange={handleInput}
+            value={formData.data}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div> */}
+
+      </div>
+  );
+};
+
+export default TimerSettings;
